8a1cc27ae61b6a174f4595ade6f4ffaf2edf26fc
walton.wang <82076408@qq.com>
2015-10-23 15:13:40 +0800

    1. add clean plugin 2. extract app from lib bundle 3. add statistic module

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,15 @@ var HtmlwebpackPlugin = require('html-webpack-plugin');
 
 var webpack = require('webpack');
 var merge = require('webpack-merge');
-
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var pkg = require('./package.json');
 var TARGET = process.env.npm_lifecycle_event;
 
 var ROOT_PATH = path.resolve(__dirname);
 var APP_PATH = path.resolve(ROOT_PATH, 'app');
 var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
+
+var Clean = require('clean-webpack-plugin');
 
 process.env.BABEL_ENV = process.env.npm_lifecycle_event;
 
@@ -58,10 +61,34 @@ if(TARGET === 'start' || !TARGET) {
   });
 }
 
-if(TARGET === 'build') {
+if(TARGET === 'build' || TARGET === 'stats' ) {
   module.exports = merge(common, {
+    entry: {
+      app: APP_PATH,
+      vendor: Object.keys(pkg.dependencies)
+    },
+    /* important! */
+    output: {
+      path: BUILD_PATH,
+      filename: '[name].[chunkhash].js?'
+    },
     devtool: 'source-map',
+    module: {
+      // loaders: [
+      //   {
+      //     test: /\.css$/,
+      //     loader: ExtractTextPlugin.extract('style', 'css'),
+      //     include: APP_PATH
+      //   }
+      // ]
+    },
     plugins: [
+      new Clean(['build']),
+      // new ExtractTextPlugin('styles.[chunkhash].css'),
+      new webpack.optimize.CommonsChunkPlugin(
+        'vendor',
+        '[name].[chunkhash].js'
+      ),
       new webpack.DefinePlugin({
         'process.env': {
           // This affects react lib size
